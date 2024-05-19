b35e89b9bfc12f1b55d3fd7544b96363c0161057
mihailsarenko <mixail.sharenko@gmail.com>
2024-05-19 17:08:04 +0500

    secind commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import React, {useState} from 'react';
 import Review from './Review';
 import people from './data'
-import {FaBeer} from "react-icons/fa";
+import {FaChevronLeft, FaChevronRight, FaQuoteRight} from "react-icons/fa";
 
 function App() {
     const [index, setIndex] = useState(0);
     const {name, job, image, text} = people[index];
-    return <div>
-        <h2>
-            reviews project setup
-        </h2>
-        <FaBeer className='beer'/>
-    </div>;
+    return <main>
+        <article className='review'>
+            <div className='img-container'>
+                <img src={image} alt={name} className='person-img'/>
+                <span className='quote-icon'>
+                    <FaQuoteRight />
+                </span>
+            </div>
+            <h4 className='author'>{name}</h4>
+            <p className='job'>{job}</p>
+            <p className='info'>{text}</p>
+        </article>
+    </main>;
 }
 
 export default App;
