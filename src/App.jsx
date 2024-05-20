a671fd518bc04f86821a08ea90f5933cd2e298ac
mihailsarenko <mixail.sharenko@gmail.com>
2024-05-21 00:08:39 +0500

    Third commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,29 @@ import {FaChevronLeft, FaChevronRight, FaQuoteRight} from "react-icons/fa";
 function App() {
     const [index, setIndex] = useState(0);
     const {name, job, image, text} = people[index];
+
+    const checkNumber = (number) => {
+        if (number > people.length - 1) {
+            return 0;
+        }
+        if (number < 0) {
+            return people.length - 1;
+        }
+        return number;
+    }
+
     const prevPerson = () => {
         setIndex((currentIndex) => {
             let newIndex =  currentIndex + 1;
-            if (newIndex > people.length - 1) {
-                return 0;
-            }
-            return newIndex;
+
+            return checkNumber(newIndex);
         })
     }
     const nextPerson = () => {
         setIndex((currentIndex) => {
             let newIndex =  currentIndex + 1;
-            if (newIndex < people.length - 1) {
-                return 0;
-            }
-            return newIndex;
+
+            return checkNumber(newIndex);
         })
     }
 
