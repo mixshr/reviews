5068ca7a8038c313e0226ebf6f6d3e1590e6993f
mihailsarenko <mixail.sharenko@gmail.com>
2024-05-20 23:50:19 +0500

    Third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React, {useState} from 'react';
-import Review from './Review';
 import people from './data'
 import {FaChevronLeft, FaChevronRight, FaQuoteRight} from "react-icons/fa";
 
 function App() {
     const [index, setIndex] = useState(0);
     const {name, job, image, text} = people[index];
+    const prevPerson = () => {
+        setIndex((currentIndex) => {
+            return currentIndex + 1;
+        })
+    }
+    const nextPerson = () => {
+        setIndex((currentIndex) => {
+            return currentIndex - 1;
+        })
+    }
+
     return <main>
         <article className='review'>
             <div className='img-container'>
@@ -17,6 +27,14 @@ function App() {
             <h4 className='author'>{name}</h4>
             <p className='job'>{job}</p>
             <p className='info'>{text}</p>
+            <div className="btn-container">
+                <button type='button' className='prev-btn' onClick={prevPerson}>
+                    <FaChevronLeft />
+                </button>
+                <button type='button' className='next-btn' onClick={nextPerson}>
+                    <FaChevronRight />
+                </button>
+            </div>
         </article>
     </main>;
 }
